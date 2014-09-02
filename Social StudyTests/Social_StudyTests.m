126f4f85deb3c1017ece0e7da06dfbc496ff851f
Nicholas Gordon <yankeenjg@Nicholass-MacBook-Pro.local>
2014-09-02 01:09:16 -0700

    commit

---
+++ Social StudyTests/Social_StudyTests.m
@@ -0,0 +1,34 @@
+//
+//  Social_StudyTests.m
+//  Social StudyTests
+//
+//  Created by Nicholas Gordon on 8/29/14.
+//  Copyright (c) 2014 Duke University. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Social_StudyTests : XCTestCase
+
+@end
+
+@implementation Social_StudyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
